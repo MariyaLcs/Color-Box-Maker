3ea12e8d0d884d56646ea6b36943a3a6e74f129d
Mariya <mariya.podosinova@gmail.com>
2020-05-21 15:09:52 +0100

    Removing Color Boxes

--- src/BoxList.js
+++ src/BoxList.js
@@ -5,16 +5,28 @@ import NewBoxForm from "./NewBoxForm";
 class BoxList extends Component {
   constructor(props) {
     super(props);
-    this.state = { boxes: [{ width: 10, height: 40, color: "orange" }] };
+    this.state = { boxes: [] };
     this.create = this.create.bind(this);
   }
   create(newBox) {
     this.setState({ boxes: [...this.state.boxes, newBox] });
   }
+  remove(id) {
+    this.setState({
+      boxes: this.state.boxes.filter((box) => box.id !== id),
+    });
+  }
 
   render() {
     const boxes = this.state.boxes.map((box) => (
-      <Box width={box.width} height={box.height} color={box.color} />
+      <Box
+        key={box.id}
+        id={box.id}
+        width={box.width}
+        height={box.height}
+        color={box.color}
+        removeBox={() => this.remove(box.id)}
+      />
     ));
 
     return (
