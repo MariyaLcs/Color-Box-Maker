a837f5c3a7bfbd55c4c589071cb0cf76b52fca2b
Mariya <mariya.podosinova@gmail.com>
2020-05-21 13:09:56 +0100

    Creating the BoxForm Component

--- src/BoxList.js
+++ src/BoxList.js
@@ -1,18 +1,16 @@
 import React, { Component } from "react";
 import Box from "./Box";
-// import NewBoxForm from "./NewBoxForm";
+import NewBoxForm from "./NewBoxForm";
 
 class BoxList extends Component {
   constructor(props) {
     super(props);
     this.state = { boxes: [{ width: 10, height: 40, color: "orange" }] };
-    //   this.handleClick = this.handleClick.bind(this);
+    this.create = this.create.bind(this);
   }
-
-  // handleClick(evt) {
-  //   // call up to the board to flip cells around this cell
-  //   this.props.flipCellsAroundMe();
-  // }
+  create(newBox) {
+    this.setState({ boxes: [...this.state.boxes, newBox] });
+  }
 
   render() {
     const boxes = this.state.boxes.map((box) => (
@@ -22,6 +20,7 @@ class BoxList extends Component {
     return (
       <div>
         <h1>Color Box</h1>
+        <NewBoxForm createBox={this.create} />
         {boxes}
       </div>
     );
